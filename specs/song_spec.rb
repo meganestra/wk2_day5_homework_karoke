c460c67e792e4aa45c3edabeb1736666722c7f61
Megan Strachan <m.e.strachan@gmail.com>
2016-06-17 10:58:16 +0100

    pass initial song tests

---
+++ specs/song_spec.rb
@@ -0,0 +1,19 @@
+require('minitest/autorun')
+require_relative('../song')
+
+class SongTest < MiniTest::Test
+
+  def setup
+    @song_1 = Song.new("LemmeTellYa", "Kidaf")
+  end
+
+  def test_song_has_song_title
+    assert_equal("LemmeTellYa", @song_1.song_title)
+  end
+
+  def test_song_has_artist_name
+    assert_equal("Kidaf", @song_1.artist_name)
+  end
+
+
+end
