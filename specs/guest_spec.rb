343b540a6cfcd6b93cf08868fc0a94a0c39f5f8c
Megan Strachan <m.e.strachan@gmail.com>
2016-06-17 10:47:38 +0100

    initial commit

---
+++ specs/guest_spec.rb
@@ -0,0 +1,14 @@
+require('minitest/autorun')
+require_relative('../guest')
+
+class GuestTest < MiniTest::Test
+
+  def setup
+    @guest_1 = Guest.new("Megan")
+  end
+
+  def test_guest_has_name
+    assert_equal("Megan", @guest_1.name)
+  end
+
+end
